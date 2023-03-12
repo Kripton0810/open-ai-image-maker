5bcd4e8875a8b6b1e8e8660f8cb950b09aeaa6d3
Kripton0810 <subhankar0810@gmail.com>
2023-03-12 18:43:31 +0530

    added works like radio button and inc butt

---
+++ src/reducers/loadingApi.reducer.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit"
+const initialState = {
+  isLoading: false,
+  isFirst: false,
+}
+
+export const loadingApiSlice = createSlice({
+  name: "loadingApi",
+  initialState,
+  reducers: {
+    isFirstSetter: (state) => {
+      if (state.isFirst == false) {
+        state.isFirst = true
+      }
+    },
+
+    isLoadingSetterOn: (state) => {
+      state.isLoading = true
+    },
+    isLoadingSetterOff: (state) => {
+      state.isLoading = false
+    },
+  },
+})
+
+export const { isFirstSetter, isLoadingSetterOn, isLoadingSetterOff } =
+  loadingApiSlice.actions
+
+export default loadingApiSlice.reducer
